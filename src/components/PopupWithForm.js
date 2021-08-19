2b7a4cade0f5ce67df3a4b356f4c00a17642daec
Wunshh <81330170+Wunshh@users.noreply.github.com>
2021-08-19 07:49:52 +0300

    fix: исправить замечания 2-й итерации

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,7 +4,7 @@ function PopupWithForm(props) {
         <div className={`popup ${props.isOpen && "popup_open"} popup_type_${props.name}`}>
             <div className="popup__container">
                 <button type="button" className="popup__close-button" onClick={props.onClose}></button>
-                <form className={`form form_${props.name}`} name="submitForm">
+                <form className={`form form_${props.name}`} name={props.name}>
                     <h2 className="form__title">{props.title}</h2>
                     {props.children}
                     <button type="submit" className="form__save-button">{props.buttonText}</button>
